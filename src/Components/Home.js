fc3fcc757ed7d66acc9dd7f0ed7d9546575e127b
LunaBlazee <162085668+LunaBlazee@users.noreply.github.com>
2024-03-12 14:19:51 +0500

    Add files via upload

---
+++ src/Components/Home.js
@@ -0,0 +1,37 @@
+import React from "react";
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi";
+import { Height } from "@mui/icons-material";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Food Trading
+          </h1>
+          <p className="primary-text">
+            <h2>Rice, Grains, Pulses</h2>
+          We provide food products and trading services ranging
+          from purchasing to private label production.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt=""/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
